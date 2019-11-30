6ef03ccfec0452512f8399707a384e5109e3e8af
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2019-11-29 21:15:56 -0500

    :sparkles: Introducing new features like CarouselView on Home, bug after login screen on HomeFragment

--- app/src/main/java/com/shibuyaxpress/petchaserkt/components/fragments/HomeFragment.kt
+++ app/src/main/java/com/shibuyaxpress/petchaserkt/components/fragments/HomeFragment.kt
@@ -4,18 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.size
 import androidx.fragment.app.Fragment
 import com.shibuyaxpress.petchaserkt.R
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
 
 class HomeFragment: Fragment() {
 
+    private var carouselView: CarouselView? = null
+    private var imageList = intArrayOf(R.drawable.car_img_1, R.drawable.car_img_2, R.drawable.car_img_3)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         /*return super.onCreateView(inflater, container, savedInstanceState)*/
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        setCarouselView(rootView)
+        return rootView
+    }
+
+    private fun setCarouselView(view:View) {
+        carouselView = view.findViewById(R.id.carouselView)
+        carouselView!!.setImageListener(imageListener)
+        carouselView!!.pageCount = imageList.size
+        carouselView!!.setAnimateOnBoundary(true)
+    }
+
+    private var imageListener: ImageListener = ImageListener { position, imageView ->
+        imageView.setImageResource(imageList[position])
     }
 
     companion object {
