c642a976698060351f738e58547fbbeec2353461
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2019-12-05 23:44:41 -0500

    :boom: Introducing breaking changes need help to display pets on maps .

--- app/src/main/java/com/shibuyaxpress/petchaserkt/components/activities/ProfileActivity.kt
+++ app/src/main/java/com/shibuyaxpress/petchaserkt/components/activities/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.shibuyaxpress.petchaserkt.components.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.shibuyaxpress.petchaserkt.R
 import kotlinx.android.synthetic.main.activity_profile.*
@@ -13,6 +14,7 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         btnLogOut.setOnClickListener {
+            Log.d("Current User logged",FirebaseAuth.getInstance().currentUser.toString())
             FirebaseAuth.getInstance().signOut()
             finishAffinity()
             startActivity(Intent(this@ProfileActivity, LoginActivity::class.java))
--- app/src/main/java/com/shibuyaxpress/petchaserkt/network/APIServiceGenerator.kt
+++ app/src/main/java/com/shibuyaxpress/petchaserkt/network/APIServiceGenerator.kt
@@ -2,15 +2,22 @@ package com.shibuyaxpress.petchaserkt.network
 
 import android.util.Log
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object APIServiceGenerator {
 
-    val petchaserClient by lazy {
+    val petchaserClient: PetChaserAPI by lazy {
         Log.d("WebAccess","Creating Retrofit Client")
+
+        var client = OkHttpClient.Builder()
+            .addInterceptor(ServiceInterceptor())
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(PetChaserAPI.API_BASE_URL)
+            .client(client)
             .addConverterFactory(MoshiConverterFactory.create())
             .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
